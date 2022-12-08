f449140a6cd88624baa08682e5bc012ea4ea7b1d
pushpak <pushpak99311@gmail.com>
2022-12-08 12:32:35 +0530

    reverse an array added

--- array/reverse_array.js
+++ array/reverse_array.js
@@ -1,6 +1,23 @@
 // Reverse the given array.
 let arr = [ 0, 1, 4, 9, 16, 25 ]   // given array
 
+//Method 1 using while loop
+function swapReverse(arr){
+    let i=0
+    let j = arr.length-1
+    while(i<j){
+        [arr[i],arr[j]] = [arr[j],arr[i]]
+        i++
+        j--
+    }
+    return arr
+}
+
+console.log(swapReverse(arr))
+
+
+
+//Method 2 using reccursion
 function reverse(arr){
     if(arr.length<=1) return arr
     return reverse(arr.slice(1)).concat(arr[0])
